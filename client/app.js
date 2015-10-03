5eabd4e8dd1d715686d6790afd94ec507276890d
briandmpls <briandmpls@gmail.com>
2015-10-03 12:50:35 -0500

    first commit

---
+++ client/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by briandaves on 10/2/15.
+ */
+var app = angular.module('to_do', []);
---
+++ server/app.js
@@ -0,0 +1,38 @@
+/**
+ * Created by briandaves on 10/2/15.
+ */
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(request,response){
+    response.sendFile(__dirname + '/public/assets/views/index.html');
+    console.log("App.get");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var server = app.listen(3000,function(){
+        var port = server.address().port;
+        console.log('Listening on port: ',port);
+});
+
+module.exports = app;
+
+
