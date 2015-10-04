88249f978251c2636caaf88b3faeb0665f5a7797
briandmpls <briandmpls@gmail.com>
2015-10-04 11:08:08 -0500

    changes to css

--- client/app.js
+++ client/app.js
@@ -1,10 +1,26 @@
 /**
  * Created by briandaves on 10/2/15.
  */
-var app = angular.module('to_do', []);
+var app = angular.module('todo', []);
 
-app.controller('ToDoController', '$scope', function($scope){
+app.controller('MainController', ['$scope', function($scope) {
+
+    $scope.toDoList=[
+    ];
 
 
+    $scope.totalToDoList = function(){
+        return $scope.toDoList.length;
+        };
 
-});
+
+    $scope.addNewItem = function () {
+        $scope.toDoList.push({text:$scope.newToDoItem, done:false});
+        $scope.newToDoItem = '';
+    };
+
+
+    $scope
+
+
+}]);
